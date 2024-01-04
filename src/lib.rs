939ae2a5838feb5a2159e64e60d30292bcb9d6ce
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-04 13:11:03 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+///
+/// Attempts to link the SDL2 window into wgpu to create a surface.
+///
+pub fn link_wgpu_to_sdl2(
+  instance: &wgpu::Instance,
+  window: &sdl2::video::Window,
+) -> Result<wgpu::Surface, wgpu::CreateSurfaceError> {
+  unsafe { instance.create_surface(window) }
+}
